3d3e0acd44be5b846a524d8e2fe628d55c3854e6
Shivam <shivamjhax@gmail.com>
2024-01-17 00:19:55 +0530

    Added the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function About() {
+export default function About() { 
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>About UrbanEstate</h1>
